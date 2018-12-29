ea7ca0f444d7d62ad52eaa4e648435b803f87661
Chris Burr <christopher.burr@cern.ch>
2018-12-29 18:49:56 +0000

    Add patches required for building ROOT

--- recipe/build_clangdev.sh
+++ recipe/build_clangdev.sh
@@ -1,3 +1,10 @@
+
+sed -i.bak -e 's@addSystemInclude(DriverArgs, CC1Args, SysRoot + "/usr/local/include");@addSystemInclude(DriverArgs, CC1Args, "'"${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/include"'");@g' \
+	    lib/Driver/ToolChains/Linux.cpp && rm $_.bak
+
+sed -i.bak -e 's@AddPath("/usr/local/include", System, false);@AddPath("'"${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/include"'", System, false);@g' \
+	    lib/Frontend/InitHeaderSearch.cpp && rm $_.bak
+
 mkdir build
 cd build
 
