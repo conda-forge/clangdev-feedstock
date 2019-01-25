04ae2469e3f497a8d2cc20e328c209bdaaad3e7d
Christopher Burr <christopher.burr@cern.ch>
2019-01-25 08:02:37 +0000

    Debug windows failure

--- recipe/build_clangdev.bat
+++ recipe/build_clangdev.bat
@@ -1,7 +1,20 @@
-if NOT "%clang_variant%" == "default" cd root-source/interpreter/llvm/src/tools/clang
+echo "clang_variant is %clang_variant%"
+ls
+if "%clang_variant%" NEQ "default" echo "If statement triggered"
+if "%clang_variant%" NEQ "default" ls root-source/interpreter/llvm/src/tools/clang
+if "%clang_variant%" NEQ "default" cd root-source/interpreter/llvm/src/tools/clang
+ls
 
+echo %cd%
+
+if "%clang_variant%" EQU "default" goto :defaultsourcedir
+cd root-source/interpreter/llvm/src/tools/clang
+:defaultsourcedir
+
+echo %cd%
 mkdir build
 cd build
+echo %cd%
 
 :: Remove -GL from CXXFLAGS as this takes too much time and memory
 set "CFLAGS= -MD"
