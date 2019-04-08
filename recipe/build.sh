13a23d1bb9a94252b4f84b863245cb658eefbc9b
Isuru Fernando <isuruf@gmail.com>
2019-04-08 18:22:22 -0500

    Try using llvmdev on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,9 @@
 mkdir build
 cd build
 
-if [[ "${target_platform}" == "osx-64" ]]; then
-    LLVM_PREFIX=`pwd`/tmp
-    conda create -p $LLVM_PREFIX -c conda-forge -c defaults --yes --quiet llvmdev=$PKG_VERSION libcxx=4.0.1
-else
-    LLVM_PREFIX=$PREFIX
-fi
-
 cmake \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_PREFIX_PATH=$LLVM_PREFIX \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_ENABLE_RTTI=ON \
   -DCLANG_INCLUDE_TESTS=OFF \
