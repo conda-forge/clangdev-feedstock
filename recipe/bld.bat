6e24530fa07a76ccad54705b79d8aaf311dbded6
Isuru Fernando <isuruf@gmail.com>
2019-04-08 13:19:07 -0500

    Allow vs2015 for now

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,12 +13,12 @@ cmake -G "Ninja" ^
     -DCLANG_INCLUDE_DOCS=OFF ^
     -DLLVM_INCLUDE_TESTS=OFF ^
     -DLLVM_INCLUDE_DOCS=OFF ^
-    -DLLVM_TARGETS_TO_BUILD=X86 ^
+    -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON ^
     %SRC_DIR%
 
 if errorlevel 1 exit 1
 
-ninja -j2
+ninja -j%CPU_COUNT%
 if errorlevel 1 exit 1
 
 ninja install
