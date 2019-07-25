9271fb3dc31c25d24c204c712b2758569ab1cfc7
Isuru Fernando <idf2@illinois.edu>
2019-07-25 10:44:07 -0500

    No clang-8 on windows

--- recipe/install_clang.bat
+++ recipe/install_clang.bat
@@ -12,6 +12,5 @@ mkdir bin
 move bin2\clang.exe bin\
 move bin2\clang-cl.exe bin\
 move bin2\clang-cpp.exe bin\
-move bin2\clang-%PKG_VERSION:~0,1%.exe bin\
 move bin2\clang++.exe bin\
 rmdir /s /q bin2
