a87aa16aaec170218194da92196cf848a84a28af
Isuru Fernando <isuruf@gmail.com>
2019-07-25 21:22:30 -0500

    Restore symlinks

--- recipe/install_clang.sh
+++ recipe/install_clang.sh
@@ -6,5 +6,9 @@ cd $PREFIX
 rm -rf lib/libclang* lib/cmake libexec share include
 mv bin bin2
 mkdir -p bin
-cp bin2/clang bin2/clang-cl bin2/clang-cpp bin2/clang-${PKG_VERSION:0:1} bin/
+cp bin2/clang-${PKG_VERSION:0:1} bin/
 rm -rf bin2
+
+ln -s bin/clang-${PKG_VERSION:0:1} bin/clang-cl
+ln -s bin/clang-${PKG_VERSION:0:1} bin/clang-cpp
+ln -s bin/clang-${PKG_VERSION:0:1} bin/clang
