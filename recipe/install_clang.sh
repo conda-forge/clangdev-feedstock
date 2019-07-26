efd4a49e15a06f32de498af29100d38c38f73ccd
Isuru Fernando <isuruf@gmail.com>
2019-07-26 11:07:17 -0500

    Make symlinks absolute

--- recipe/install_clang.sh
+++ recipe/install_clang.sh
@@ -2,13 +2,13 @@
 set -x -e
 cd ${SRC_DIR}/build
 make install
-cd $PREFIX
+cd "${PREFIX}"
 rm -rf lib/libclang* lib/cmake libexec share include
 mv bin bin2
 mkdir -p bin
 cp bin2/clang-${PKG_VERSION:0:1} bin/
 rm -rf bin2
 
-ln -s bin/clang-${PKG_VERSION:0:1} bin/clang-cl
-ln -s bin/clang-${PKG_VERSION:0:1} bin/clang-cpp
-ln -s bin/clang-${PKG_VERSION:0:1} bin/clang
+ln -s "${PREFIX}/bin/clang-${PKG_VERSION:0:1}" "${PREFIX}/bin/clang-cl"
+ln -s "${PREFIX}/bin/clang-${PKG_VERSION:0:1}" "${PREFIX}/bin/clang-cpp"
+ln -s "${PREFIX}/bin/clang-${PKG_VERSION:0:1}" "${PREFIX}/bin/clang"
