27216bae3baebb3a5a5580b27de5ea5181ea81fe
Isuru Fernando <isuruf@gmail.com>
2019-08-18 16:11:31 -0500

    Use tablegen from host

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,4 @@
-if [[ "$target_platform" == "osx-64" ]]; then
-    export CONDA_BUILD_SYSROOT_BACKUP=${CONDA_BUILD_SYSROOT}
-    conda install -p $BUILD_PREFIX --quiet --yes clangxx_osx-64=${cxx_compiler_version}
-    export CONDA_BUILD_SYSROOT=${CONDA_BUILD_SYSROOT_BACKUP}
-fi
+source osx_hack.sh
 
 mkdir build
 cd build
--- recipe/osx_hack.sh
+++ recipe/osx_hack.sh
@@ -4,4 +4,5 @@ if [[ "$target_platform" == "osx-64" ]]; then
     export CONDA_BUILD_SYSROOT_BACKUP=${CONDA_BUILD_SYSROOT}
     conda install -p $BUILD_PREFIX --quiet --yes clangxx_osx-64=${cxx_compiler_version}
     export CONDA_BUILD_SYSROOT=${CONDA_BUILD_SYSROOT_BACKUP}
+    export PATH="$PREFIX/bin:$PATH"
 fi
