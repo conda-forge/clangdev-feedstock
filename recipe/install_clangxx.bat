cf5ea514e359437d8fea9d78af30a0087ae369f6
Isuru Fernando <isuruf@gmail.com>
2020-10-16 17:08:56 -0500

    fix windows and osx

--- recipe/install_clangxx.bat
+++ recipe/install_clangxx.bat
@@ -1,2 +1,5 @@
-cd %LIBRARY_BIN%
-ren clang.exe clang++.exe
+cd %LIBRARY_PREFIX%
+setlocal enabledelayedexpansion
+for /f "tokens=1 delims=." %%a in ("%PKG_VERSION%") do (
+  copy bin\clang-%%a.exe bin\clang++.exe
+)
