d78d92357b1e87e8fddc093b406f34ad8c7db63d
Mark Harfouche <mark.harfouche@gmail.com>
2021-04-11 18:15:32 -0400

    Move files instead of copying them to preseve symlinks

--- recipe/install_libclang_cpp.sh
+++ recipe/install_libclang_cpp.sh
@@ -8,9 +8,9 @@ rm -rf libexec share bin include
 mv lib lib2
 mkdir lib
 if [[ "$PKG_NAME" == "libclang-cpp" ]]; then
-    cp lib2/${PKG_NAME}${SHLIB_EXT} lib/
+    mv lib2/${PKG_NAME}${SHLIB_EXT} lib/
 else
-    cp lib2/libclang-cpp.*.* lib/
+    mv lib2/libclang-cpp.*.* lib/
 fi
 rm -rf lib2
 
