ec8dbc1231783e267a49703e9458570995d8f178
Julian Rüth <julian.rueth@fsfe.org>
2021-08-31 16:14:39 +0200

    fix root patching on Windows

--- recipe/root-patch.sh
+++ recipe/root-patch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -ex
 
 if [[ "$variant" == "root"* ]]; then
   # Cling needs some minor patches to the LLVM sources
-  sed -i "s@LLVM_LINK_LLVM_DYLIB yes@LLVM_LINK_LLVM_DYLIB no@g" "${PREFIX}/lib/cmake/llvm/LLVMConfig.cmake"
-  cd "${PREFIX}"
+  sed -i "s@LLVM_LINK_LLVM_DYLIB yes@LLVM_LINK_LLVM_DYLIB no@g" "${LIBRARY_PREFIX:-$PREFIX}/lib/cmake/llvm/LLVMConfig.cmake"
+  cd "${LIBRARY_PREFIX:-$PREFIX}"
   patch -p1 < "${RECIPE_DIR}/patches/root/llvm/0001-Fix-the-compilation.patch"
   patch -p1 < "${RECIPE_DIR}/patches/root/llvm/0002-Make-datamember-protected.patch"
   cd -
