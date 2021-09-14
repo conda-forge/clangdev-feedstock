07b6144bd85e06dc6e1bb918b0ddab7c6a8025c3
Isuru Fernando <isuruf@gmail.com>
2021-09-13 22:09:02 -0500

    Fix building clang-format package

--- recipe/install_clang_tools.sh
+++ recipe/install_clang_tools.sh
@@ -6,5 +6,7 @@ cd $PREFIX
 rm -rf lib/cmake include lib/lib*.a
 MAJOR_VERSION=$(echo ${PKG_VERSION} | cut -f1 -d".")
 for f in ${PREFIX}/bin/clang-*; do
-    ln -s $f $(basename $f)-${MAJOR_VERSION};
+    rm -f $(basename $f)-${MAJOR_VERSION}
+    mv $f $(basename $f)-${MAJOR_VERSION};
+    ln -s $(basename $f)-${MAJOR_VERSION} $f;
 done
