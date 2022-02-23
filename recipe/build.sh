65afb6d3676db33be02f72cdc527696c0afaca42
H. Vetinari <h.vetinari@gmx.com>
2022-02-23 17:13:45 +1100

    remove -fno-plt like for llvmdev for GCC >9 on ppc
    [backport of 8d12866510abb97d0c9308137c9730e9696bcba9]

--- recipe/build.sh
+++ recipe/build.sh
@@ -45,6 +45,13 @@ if [[ "$target_platform" == osx* ]]; then
   export CXXFLAGS="$CXXFLAGS -DTARGET_OS_OSX=1"
 fi
 
+# disable -fno-plt due to some GCC bug causing linker errors, see
+# https://github.com/llvm/llvm-project/issues/51205
+if [[ "$target_platform" == "linux-ppc64le" ]]; then
+  CFLAGS="$(echo $CFLAGS | sed 's/-fno-plt //g')"
+  CXXFLAGS="$(echo $CXXFLAGS | sed 's/-fno-plt //g')"
+fi
+
 mkdir build
 cd build
 
