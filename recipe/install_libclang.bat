11599869fdbf8fcb743c8242cdbd1e92dbf0d52c
H. Vetinari <h.vetinari@gmx.com>
2022-04-14 12:22:23 +1100

    add libclang_soversion uniformly across platforms

--- recipe/install_libclang.bat
+++ recipe/install_libclang.bat
@@ -1,3 +1,5 @@
+@echo on
+
 cd %SRC_DIR%\build
 ninja install
 if %ERRORLEVEL% neq 0 exit 1
@@ -9,5 +11,14 @@ move bin bin2
 
 mkdir bin
 
-move bin2\libclang.dll bin\
+setlocal enabledelayedexpansion
+if "%PKG_NAME%"=="libclang" (
+    REM unversioned
+    move bin2\libclang.dll bin\
+) else (
+    REM versioned
+    for /f "tokens=1 delims=." %%a in ("%libclang_soversion%") do (
+        move bin2\libclang.dll bin\libclang-%%a.dll
+    )
+)
 rmdir /s /q bin2
