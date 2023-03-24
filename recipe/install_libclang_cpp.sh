a5c3dffa973dedb41d9d20ec93ac7eca265888e3
Isuru Fernando <isuruf@gmail.com>
2023-03-24 01:31:43 -0500

    don't delete files for better post link checks

--- recipe/install_libclang_cpp.sh
+++ recipe/install_libclang_cpp.sh
@@ -3,15 +3,3 @@ set -ex
 
 cd ${SRC_DIR}/clang/build
 make install VERBOSE=1
-
-cd $PREFIX
-rm -rf libexec share bin include
-mv lib lib2
-mkdir lib
-
-if [[ "$PKG_NAME" == "libclang-cpp" ]]; then
-    mv lib2/${PKG_NAME}${SHLIB_EXT} lib/
-else
-    mv lib2/libclang-cpp.*.* lib/
-fi
-rm -rf lib2
