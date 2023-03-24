c0d0ecbba1aaeeeedecbc99b82ae29201e5de873
Isuru Fernando <isuruf@gmail.com>
2023-03-24 13:12:44 -0500

    fix clang-format version

--- recipe/install_clang_format.sh
+++ recipe/install_clang_format.sh
@@ -4,6 +4,7 @@ set -ex
 cd ${SRC_DIR}/clang/build
 make install
 
+MAJOR_VERSION=$(echo ${PKG_VERSION} | cut -f1 -d".")
 mv ${PREFIX}/bin/clang-format ${PREFIX}/bin/clang-format-${MAJOR_VERSION}
 
 if [[ "$PKG_NAME" == "clang-format" ]]; then
