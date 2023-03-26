12feaadb421fbfaedeb5cd8bafdfaa7c19fc0c57
H. Vetinari <h.vetinari@gmx.com>
2023-03-27 02:14:46 +1100

    Revert "fix installation of clang-format"
    Now that we correctly have clang-format-16 present after the
    jinja string concat shenanigans.
    
    This reverts commit f7fcf2e7ed983ea373c709c1e6770005344cbca2.

--- recipe/install_clang_format.sh
+++ recipe/install_clang_format.sh
@@ -5,12 +5,8 @@ cd ${SRC_DIR}/clang/build
 make install
 
 MAJOR_VERSION=$(echo ${PKG_VERSION} | cut -f1 -d".")
+mv ${PREFIX}/bin/clang-format ${PREFIX}/bin/clang-format-${MAJOR_VERSION}
 
 if [[ "$PKG_NAME" == "clang-format" ]]; then
-  # this branch gets executed _after_ the one below,
-  # so there's a versioned binary to symlink to
-  rm ${PREFIX}/bin/clang-format
   ln -sf $PREFIX/bin/clang-format-${MAJOR_VERSION} $PREFIX/bin/clang-format
-else
-  mv ${PREFIX}/bin/clang-format ${PREFIX}/bin/clang-format-${MAJOR_VERSION}
 fi
