fb8d79134017646c7a0ef89386899b9a0fb44fd9
Isuru Fernando <isuruf@gmail.com>
2023-03-27 01:17:38 -0500

    fix clang-tools clobbering

--- recipe/install_clang_tools.bat
+++ recipe/install_clang_tools.bat
@@ -3,3 +3,6 @@
 cd %SRC_DIR%\clang\build
 ninja install
 if %ERRORLEVEL% neq 0 exit 1
+
+cd %LIBRARY_PREFIX%
+rmdir /s /q include\clang include\clang-c include\clang-tidy lib
--- recipe/install_clang_tools.sh
+++ recipe/install_clang_tools.sh
@@ -5,6 +5,13 @@ cd ${SRC_DIR}/clang/build
 make install
 cd $PREFIX
 
+# Remove stuff that should be in clangdev
+rm -rf include/clang
+rm -rf include/clang-c
+rm -rf include/clang-tidy
+rm -rf lib/cmake/clang/
+rm -rf lib/lib*.a
+
 MAJOR_VERSION=$(echo ${PKG_VERSION} | cut -f1 -d".")
 for f in ${PREFIX}/bin/clang-*; do
     if [[ "$(basename $f)" == clang-format-* ]]; then
