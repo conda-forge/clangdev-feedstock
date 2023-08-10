946e1562704682ee4593191b6107ce0522e512c8
H. Vetinari <h.vetinari@gmx.com>
2023-08-10 15:42:53 +1100

    set LLVM_ENABLE_LIBCXX=ON on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -42,6 +42,7 @@ fi
 
 if [[ "$target_platform" == osx* ]]; then
   export CXXFLAGS="$CXXFLAGS -DTARGET_OS_OSX=1"
+  CMAKE_ARGS="$CMAKE_ARGS -DLLVM_ENABLE_LIBCXX=ON"
 fi
 
 # disable -fno-plt due to some GCC bug causing linker errors, see
