c897a6da29cde694909d9db4bf3d5f552bed24a0
H. Vetinari <h.vetinari@gmx.com>
2023-11-13 10:16:10 +1100

    add comment about sysroot also in install_clang_symlinks.sh

--- recipe/install_clang_symlinks.sh
+++ recipe/install_clang_symlinks.sh
@@ -7,5 +7,6 @@ if [[ "$target_platform" == "linux-"* ]]; then
   source ${RECIPE_DIR}/get_cpu_triplet.sh
   CHOST=$(get_triplet $target_platform)
   ln -s "${PREFIX}/bin/clang-${maj_version}" "${PREFIX}/bin/${CHOST}-clang"
+  # for background, see comment in install_clangxx.sh
   echo "--sysroot ${PREFIX}/${CHOST}/sysroot" >> ${PREFIX}/bin/${CHOST}-clang.cfg
 fi
