cae550f2c564eed27656080f49aa244c2c5cea41
Isuru Fernando <isuruf@gmail.com>
2023-11-12 11:30:00 -0600

    Update comment in recipe/install_clangxx.sh
    Co-authored-by: h-vetinari <h.vetinari@gmx.com>

--- recipe/install_clangxx.sh
+++ recipe/install_clangxx.sh
@@ -11,9 +11,11 @@ if [[ "$target_platform" == "linux-"* ]]; then
   source ${RECIPE_DIR}/get_cpu_triplet.sh
   CHOST=$(get_triplet $target_platform)
   ln -s "${PREFIX}/bin/clang++" "${PREFIX}/bin/${CHOST}-clang++"
-  # In cross compiling case we set CONDA_BUILD_SYSROOT to host platform
+  # In the cross compiling case, we set CONDA_BUILD_SYSROOT to host platform
   # which makes compiling for build platform not work correctly.
-  # This overrides CONDA_BUILD_SYSROOT so that native compilation
-  # works correctly in cross compilation scenarios.
+  # The following overrides CONDA_BUILD_SYSROOT, so that a clang for a given
+  # CHOST will always use the appropriate sysroot. In particular, this means
+  # that a clang in the build environment will work correctly for its native
+  # architecture also in cross compilation scenarios.
   echo "--sysroot ${PREFIX}/${CHOST}/sysroot" >> ${PREFIX}/bin/${CHOST}-clang++.cfg
 fi
