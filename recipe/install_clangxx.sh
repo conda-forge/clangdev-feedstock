89a29c838fd7182ec05d4fe63951653d47354cfa
H. Vetinari <h.vetinari@gmx.com>
2023-11-13 10:27:28 +1100

    remove unused symlink

--- recipe/install_clangxx.sh
+++ recipe/install_clangxx.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ln -s $PREFIX/bin/clang  $PREFIX/bin/clang++
-ln -s $PREFIX/bin/clang  $PREFIX/bin/$HOST-clang++
 
 if [[ "$variant" == "hcc" ]]; then
   ln -s $PREFIX/bin/clang++  $PREFIX/bin/hcc
