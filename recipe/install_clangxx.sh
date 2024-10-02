15f25fb6de4f03aa9e2efef5941bc2bc9cdb2729
H. Vetinari <h.vetinari@gmx.com>
2024-10-02 07:39:54 +0200

    remove some more hcc leftovers

--- recipe/install_clangxx.sh
+++ recipe/install_clangxx.sh
@@ -3,10 +3,6 @@
 ln -s $PREFIX/bin/clang  $PREFIX/bin/clang++
 ln -s $PREFIX/bin/clang  $PREFIX/bin/$HOST-clang++
 
-if [[ "$variant" == "hcc" ]]; then
-  ln -s $PREFIX/bin/clang++  $PREFIX/bin/hcc
-fi
-
 if [[ "$target_platform" == "linux-"* ]]; then
   source ${RECIPE_DIR}/get_cpu_triplet.sh
   CHOST=$(get_triplet $target_platform)
