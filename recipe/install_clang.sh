363d39673aefa70ce7235804892008ecc08d42f0
h-vetinari <h.vetinari@gmx.com>
2025-03-18 06:56:58 +1100

    remove stray quote

--- recipe/install_clang.sh
+++ recipe/install_clang.sh
@@ -6,7 +6,7 @@ make install
 
 MAJOR_VERSION=$(echo ${PKG_VERSION} | cut -f1 -d".")
 if [[ ! -d $PREFIX/lib/clang/${MAJOR_VERSION}/include ]]; then
-  echo "$PREFIX/lib/clang/${MAJOR_VERSION}/include" not found"
+  echo "$PREFIX/lib/clang/${MAJOR_VERSION}/include not found"
   exit 1
 fi
 # Make sure omp.h from conda environment is found by clang
