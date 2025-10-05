fe579838374a821a0b5efaa17c7ca91202800f38
Isuru Fernando <isuruf@gmail.com>
2025-10-05 17:47:55 -0500

    fix osx and windows

--- recipe/install_clang.bat
+++ recipe/install_clang.bat
@@ -8,13 +8,13 @@ setlocal enabledelayedexpansion
 for /f "tokens=1 delims=." %%a in ("%PKG_VERSION%") do (
   set "MAJOR_VERSION=%%a"
 )
-FOR /F "tokens=* USEBACKQ" %%F IN (`%LIBRARY_PREFIX%\bin\clang-!MAJOR_VERSION!.exe -print-resource-dir`) DO (
-   set "RESOURCE_DIR=%%F"
+FOR /F "tokens=* USEBACKQ" %%F IN (`%LIBRARY_PREFIX%\bin\clang.exe -print-resource-dir`) DO (
+  set "RESOURCE_DIR=%%F"
 )
 set "RESOURCE_DIR_REF=%LIBRARY_LIB:/=\%\clang\!MAJOR_VERSION!"
 if NOT "!RESOURCE_DIR!" == "!RESOURCE_DIR_REF!" (
-    echo "resource dir !RESOURCE_DIR_REF! does not match expected !RESOURCE_DIR!"
-    exit 1
+  echo "resource dir !RESOURCE_DIR_REF! does not match expected !RESOURCE_DIR!"
+  exit 1
 )
 :: Make sure omp.h from conda environment is found by clang
 copy %LIBRARY_PREFIX%\include\omp.h %RESOURCE_DIR_REF%\include\omp.h
