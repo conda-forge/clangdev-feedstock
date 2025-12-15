c723abdfea4dbb10a6df48e26f0048a76abb66d9
Isuru Fernando <isuruf@gmail.com>
2025-12-15 11:10:00 -0800

    move clang_impl for native compilers here
    these packages are in activation feedstocks, but the way they are
    setup, they result in clobberring each other because if you
    install both `clang_impl_osx-arm64` and `clang_osx-64`, you clobber
    `x86_64-apple-darwin13.4.0-clang`.
    
    This also allows us to add target specific clang configuration files
    that `clang` package can depend on in the future.

--- recipe/install_clang_symlinks.bat
+++ recipe/install_clang_symlinks.bat
@@ -2,8 +2,16 @@
 
 cd %LIBRARY_BIN%
 setlocal enabledelayedexpansion
-for /f "tokens=1 delims=." %%a in ("%PKG_VERSION%") do (
-   copy clang-%%a.exe clang.exe
-   copy clang-%%a.exe clang-cl.exe
-   copy clang-%%a.exe clang-cpp.exe
+if "%PKG_NAME%" == "clang" (
+  for /f "tokens=1 delims=." %%a in ("%PKG_VERSION%") do (
+    copy clang-%%a.exe clang.exe
+    copy clang-%%a.exe clang-cl.exe
+    copy clang-%%a.exe clang-cpp.exe
+  )
+)
+
+if "%PKG_NAME%" == clangxx" (
+  for /f "tokens=1 delims=." %%a in ("%PKG_VERSION%") do (
+    copy clang-%%a.exe clang++.exe
+  )
 )
