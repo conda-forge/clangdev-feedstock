079aff0fdd29083da9683f64367acc4e412de6ae
Isuru Fernando <isuruf@gmail.com>
2025-12-17 07:39:30 -0800

    fix syntax

--- recipe/install_clang_symlinks.bat
+++ recipe/install_clang_symlinks.bat
@@ -4,14 +4,14 @@ cd %LIBRARY_BIN%
 setlocal enabledelayedexpansion
 if "%PKG_NAME%" == "clang" (
   for /f "tokens=1 delims=." %%a in ("%PKG_VERSION%") do (
-    copy clang-%%a.exe clang.exe
-    copy clang-%%a.exe clang-cl.exe
-    copy clang-%%a.exe clang-cpp.exe
+    copy clang-%%a.exe "clang.exe"
+    copy clang-%%a.exe "clang-cl.exe"
+    copy clang-%%a.exe "clang-cpp.exe"
   )
 )
 
 if "%PKG_NAME%" == "clangxx" (
   for /f "tokens=1 delims=." %%a in ("%PKG_VERSION%") do (
-    copy clang-%%a.exe clang++.exe
+    copy clang-%%a.exe "clang++.exe"
   )
 )
