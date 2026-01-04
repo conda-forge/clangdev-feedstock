2b5fe9c93dac33c9e4b5ab7680b3ea41b6044b60
Isuru Fernando <isuruf@gmail.com>
2026-01-03 22:52:09 -0800

    Fix openmp on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -51,3 +51,18 @@ for /f "tokens=1 delims=." %%a in ("%PKG_VERSION%") do (
   copy "%LIBRARY_BIN%\\clang.exe" "%LIBRARY_BIN%\\clang-%%a.exe"
   copy "%LIBRARY_BIN%\\clang.exe" "%LIBRARY_BIN%\\clang++-%%a.exe"
 )
+
+setlocal enabledelayedexpansion
+for /f "tokens=1 delims=." %%a in ("%PKG_VERSION%") do (
+  set "MAJOR_VERSION=%%a"
+)
+FOR /F "tokens=* USEBACKQ" %%F IN (`%LIBRARY_PREFIX%\bin\clang.exe -print-resource-dir`) DO (
+  set "RESOURCE_DIR=%%F"
+)
+set "RESOURCE_DIR_REF=%LIBRARY_LIB:/=\%\clang\!MAJOR_VERSION!"
+if NOT "!RESOURCE_DIR!" == "!RESOURCE_DIR_REF!" (
+  echo "resource dir !RESOURCE_DIR_REF! does not match expected !RESOURCE_DIR!"
+  exit 1
+)
+:: Make sure omp.h from conda environment is found by clang
+copy %LIBRARY_PREFIX%\include\omp.h %RESOURCE_DIR_REF%\include\omp.h
