5e2c5d8b0d15f7575acaf8cd3d5c29a9b84ca509
trim21 <i@trim21.me>
2026-07-12 07:16:32 +0800

    Add git-clang-format to clang-format package

--- recipe/install_clang_format.sh
+++ recipe/install_clang_format.sh
@@ -10,3 +10,9 @@ mv ${PREFIX}/bin/clang-format ${PREFIX}/bin/clang-format-${MAJOR_VERSION}
 if [[ "$PKG_NAME" == "clang-format" ]]; then
   ln -sf $PREFIX/bin/clang-format-${MAJOR_VERSION} $PREFIX/bin/clang-format
 fi
+
+# Install git-clang-format
+if [[ "$PKG_NAME" == "clang-format" ]]; then
+  cp ${SRC_DIR}/clang/tools/clang-format/git-clang-format ${PREFIX}/bin/
+  chmod +x ${PREFIX}/bin/git-clang-format
+fi
